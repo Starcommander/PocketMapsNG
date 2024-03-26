b186bde1ea7919e553c47a332b5fb7216737c244
001121578 <paul.kashofer@engel.at>
2024-03-26 12:07:43 +0100

    Allow to disable/dispose maps. Added actor as arg to listener for clicks. Cleanup confusing map-actions

--- pocketmaps/core/src/com/starcom/interfaces/IClickListener.java
+++ pocketmaps/core/src/com/starcom/interfaces/IClickListener.java
@@ -3,5 +3,9 @@ package com.starcom.interfaces;
 /** A ClickListener that can be wrapped via Util.wrapClickListener(l); */
 public interface IClickListener
 {
-	public void click(float x, float y);
+	/** Executed when click is done.
+	 * @param actor The actor that is listening.
+	 * @param x The X position relative to actor
+	 * @param y The Y position relative to actor */
+	public void click(Object actor, float x, float y);
 }
